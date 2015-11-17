47c56b51f462c5bc68a3891671b9318308548181
Ewerton Costa <ewertoncosta@macbook-pro-de-ewerton.local>
2015-11-16 22:07:07 -0200

    Initial Commit

---
+++ 1175/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1175
+//
+//  Created by Ewerton Costa on 16/11/15.
+//  Copyright © 2015 Ewerton Costa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
