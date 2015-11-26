a7e52004413e76c527722e4a1874e9095f3fe230
EWERTONCOSTA <ewertonxcosta@gmail.com>
2015-11-26 00:11:16 -0200

    Finalizado
    Eliminando hello world

--- 1175/main.c
+++ 1175/main.c
@@ -11,5 +11,15 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+int main() {
+    int N[20], i, j=0;
+    
+    for(i=0;i<20;i++){
+        scanf("%d",&N[i]);
+    }
+    for(i=19;i>=0;i--){
+        printf("N[%d] = %d\n",j, N[i]);
+        j++;
+    }
     return 0;
 }
