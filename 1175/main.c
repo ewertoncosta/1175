70066ec6373c9a6d21d8ca98304ef11b870b77a8
Ewerton Costa <ewertonxcosta@gmail.com>
2015-11-26 00:21:41 -0200

    Comentários
    Adicionado comentários ao código

--- 1175/main.c
+++ 1175/main.c
@@ -6,17 +6,17 @@
 //  Copyright © 2015 Ewerton Costa. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main() {
-    int N[20], i, j=0;
-    
-    for(i=0;i<20;i++){
-        scanf("%d",&N[i]);
+#include<stdio.h>
+int main(){
+    int N[20], i, a = 0;
+    //Pede 20 valores
+    for(i = 0; i < 20; i++){
+        scanf("%d", &N[i]);
     }
-    for(i=19;i>=0;i--){
-        printf("N[%d] = %d\n",j, N[i]);
-        j++;
+    //Imprime os 20 ao contrario
+    for(i=19 ; i >=0 ; i--){
+    printf("N[%d] = %d\n", a, N[i]);
+    a++;
     }
     return 0;
 }
